5f9fc9e84d63ac7ad6fd5a06cc953375eb7059f2
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-02-09 14:06:18 +0100

    Zadanie, dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Adam";
+var sex = 'm';
+var age = 16;
+
+if(age < 30 && sex == 'k')
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+
+if (age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+if (age < 18 && sex == 'm')
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}

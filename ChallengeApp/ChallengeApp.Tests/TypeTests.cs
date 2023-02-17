043683a43201d25f74e68456b6a27cbb420c939c
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-02-17 18:20:18 +0100

    Zadanie, dzień 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,38 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TEST()
+        {
+            //arrange
+            var number1 = 1;
+            var number2 = 1;
+
+            //act
+
+            //assert
+            Assert.AreEqual(number1, number2);
+
+        }
+
+        [Test]
+        public void GetEmployeeShouldReturnDifferentObjects()
+        {
+            //arrange
+            var employee1 = GetEmployee("Adam", "Nowak", 30);
+            var employee2 = GetEmployee("Tomasz", "Kowalski", 35);
+
+            //act
+
+            //assert
+            Assert.AreNotEqual(employee1, employee2);
+
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}

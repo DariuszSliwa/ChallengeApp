2857d2a888fe35ce89e774525de5175776dfe2f6
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-02-17 16:09:35 +0100

    Zadanie, dzień 7

---
+++ ChallengeApp/ChallengeApp.Tests/UserTests.cs
@@ -0,0 +1,25 @@
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenEmployeeCollectFewScores_ShouldCorrectResult()
+        {
+            //arrange
+            var employee = new Employee("Adam", "Nowakowski", 36);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(6);
+            employee.AddScore(-6);
+            employee.AddScore(-4);
+            employee.AddScore(2);
+
+            //act
+            var result = employee.Result;
+
+            //assert
+            Assert.AreEqual(9, result);
+
+        }
+    }
+}
